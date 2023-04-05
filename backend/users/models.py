e7a662e5baa96a3a47d93671026f566bc090dc52
Jasper Agustin <jasper.agustin@dynameyes.com>
2023-04-05 09:02:14 +0800

    - add registration step fields on model - define additional user properties and methods - create migration file user model changes

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+from backend.users.constants import MAX_REGISTRATION_STEP, MIN_REGISTRATION_STEP
 
 
 class User(AbstractUser):
@@ -11,6 +13,29 @@ class User(AbstractUser):
     """
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+
+    # Registration Steps
+    step1 = models.CharField(_("Registration step 1"), blank=True, max_length=255)
+    step2 = models.CharField(_("Registration step 2"), blank=True, max_length=255)
+    step3 = models.CharField(_("Registration step 3"), blank=True, max_length=255)
+
+    @property
+    def current_registration_step(self):
+        if not self.step1:
+            return 1
+        if not self.step2:
+            return 2
+        if not self.step3:
+            return 3
+        return 4
+
+    def update_registration_step(self, step, value):
+        if step < MIN_REGISTRATION_STEP or step > MAX_REGISTRATION_STEP:
+            return
+
+        step_mapping = ["", "step1", "step2", "step3"]
+        setattr(self, step_mapping[step], value)
+        self.save()
